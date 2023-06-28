c5ec0d8079e55d500b1d1a93bfc39e227e29799d
samiksha-3010 <samiksha30102@gmail.com>
2023-06-28 11:24:06 +0530

    done

--- counterapp/src/components/Type1UseEffect.jsx
+++ counterapp/src/components/Type1UseEffect.jsx
@@ -1,8 +1,15 @@
+//  import  { Compenent, useEffect, useEffect, useState} from "react";
+//  function Type1UseEffect(){
+//     const [ counter, setCounter] = useState(0);
+//     const [ samiksha,  ]
+//  }
+ 
  function  Type1UseEffect() {
     return(
         <div>
             <h1>Type1UseEffect</h1>
         </div>
+
     )
  }
  export default Type1UseEffect;
