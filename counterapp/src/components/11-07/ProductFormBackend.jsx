f185975fb05d62a7e275b116d71f44ea478f06c3
samiksha-3010 <samiksha30102@gmail.com>
2023-07-12 12:04:46 +0530

    Update ProductFormBackend.jsx

--- counterapp/src/components/11-07/ProductFormBackend.jsx
+++ counterapp/src/components/11-07/ProductFormBackend.jsx
@@ -1,44 +1,41 @@
-// import React,{useEffect, useState} from 'react'
-// // import { json } from 'react-router-dom';
+import React,{useEffect, useState} from 'react'
+// import { json } from 'react-router-dom';
 
-// const  ProductFormBackend = ()=> {
-//     const [products, setProducts] =([]);
-//     // console.log(products, "-product")
+const  ProductFormBackend = ()=> {
+    const [products, setProducts] =([]);
+    console.log(products, "-product")
 
-//     useEffect (() =>{
-//         fetch('https://fakestoreapi.com/products')
-//         .then(res => res.json())
-//         .then(json=>setProducts (json))
-//         .then (json => console.log(json))
+    useEffect (() =>{
+        fetch('https://fakestoreapi.com/products')
+        .then(res => res.json())
+        .then(json=>setProducts (json))
+        .then (json => console.log(json))
 
-//     },[])
-//   return (
-//     <div>
-//         <h2>Product:</h2>
-//         { products.length ?
-//          <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}> 
-//           { products.map((pro) =>(
-//               <div style={{ width: "18%", border: "2px solid black", height: '450px', marginBottom: "30px" }}>
-//               <img style={{ width: "80%", height: "260px", marginLeft: "10%", marginTop: "25px" }} src={pro.image} />
-//               <h4 style={{ marginLeft: "20px" }} >Name : {pro.title}</h4 >
-//               <h3 style={{ marginLeft: "20px" }}>Price : {pro.price}Rs.</h3>
-//           </div>
+    },[])
+
+
+  return (
+    <div>
+        <h2>Product:</h2>
+    { products.length ?
+         <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}> 
+          { products.map((pro) =>(
+              <div style={{ width: "18%", border: "2px solid black", height: '450px', marginBottom: "30px" }}>
+              <img style={{ width: "80%", height: "260px", marginLeft: "10%", marginTop: "25px" }} src={pro.image} />
+              <h4 style={{ marginLeft: "20px" }} >Name : {pro.title}</h4 >
+              <h3 style={{ marginLeft: "20px" }}>Price : {pro.price}Rs.</h3>
+          </div>
           
 
-//           ) )}
-         
-
-//          </div>
+          ) )}
+            </div>
+            :
+            <h1 style={{ width: "15%", margin: 'auto' }}>Loading.</h1>
         
-    
-       
+            }
+        
+  </div>
+  )
+        }
 
-
-
-//           }
-       
-//     </div>
-//   )
-// }
-
-// export default ProductFormBackend
+export default ProductFormBackend
