52afe29cd7e468986cddcfc57c71f077f1133de6
samiksha-3010 <samiksha30102@gmail.com>
2023-08-02 20:15:49 +0530

    updates

--- test/src/Component/Home.jsx
+++ test/src/Component/Home.jsx
@@ -6,9 +6,9 @@ const Home = () => {
   const {state,Logout} = useContext (AuthContext);
   // console.log(state)
   return (
-    <div>Home{state?.user?.name}
-    <button onClick={Logout}>Logout</button>
-    <img/>
+    <div><h2 style={{fontSize: "30px"}}>Home Page</h2>{state?.user?.name}<br/>
+    <button  style={{width: "20%" , height:"40px", borderRadius: "50px", backgroundColor: "black", color:"white"}} onClick={Logout}>Logout</button>
+    <img  style={{marginTop: "50px", marginLeft: "50px"}} src='https://www.w3schools.com/howto/img_nature_wide.jpg'/>
     </div>
 
   )
