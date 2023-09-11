3c2c167f1f71a7f1f95e8c9c27b231b03b9470b9
samiksha-3010 <samiksha30102@gmail.com>
2023-09-12 01:42:58 +0530

    Update DeclerativeWay.jsx

--- counterapp/src/components/01-07/DeclerativeWay.jsx
+++ counterapp/src/components/01-07/DeclerativeWay.jsx
@@ -7,11 +7,13 @@ const DeclerativeWay = () => {
         <div>
             {myData.map((product) => (
                 <div>
-                    <h1> Name :  {product.name}</h1>
+                    <h1> Name :{product.name}</h1>
                     <h2>Description :{product.description}</h2>
                     <h2>Price : {product.price} Rs.</h2>
                 </div>
             ))}
+
+            
         </div>)
 }
 export default DeclerativeWay
