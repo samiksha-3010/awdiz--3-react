33b3d310698a11791695edaf20030e82b2cf192d
samiksha-3010 <samiksha30102@gmail.com>
2023-12-15 09:41:30 +0530

    done

--- counterapp/src/components/SecondPratice/SimRegister.jsx
+++ counterapp/src/components/SecondPratice/SimRegister.jsx
@@ -1,16 +1,42 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const SimRegister = () => {
-      const[userData, setUserData] = useState({name:"",email:"",password:""})
+      const router = useNavigate();
+      const[userData, setUserData] = useState({name:"",email:"",password:"",Confirmpassword:""})
       console.log(userData,setUserData)
 
       function  handleSubmit(event){
             event.preventDefault();
-            alert (" User Registration Completed..")
-            setUserData({ name: "", email: "", password: "" })
+            if(userData.name && userData.email && userData.password && userData.Confirmpassword){
+                  if(userData.password === userData.Confirmpassword){
+                        try {
+                              // const responce = await axios.post('http://localhost:8000/register',{userData})  
+                              const responce = { data:{success:true,message:"Register Cpmplited"}}  
+                              if(responce.data.success===true){
+                                    alert (responce.data.message)
+                                    setUserData({ name: "", email: "", password: "",Confirmpassword:"" })
+                                    router ("/login-form")
+                                
+                              }                         
+                        } catch (error) {
+                              console.log(error)
+                              alert(error.responce.data.message)
+                              
+                        }
+                  }else{
+                        alert("Password And Confirm Password not match")
+                  }
+            }else{
+                  alert("All Field are Required")
+            }
+
+           
       }
       function handleChange(event)  {
             setUserData({...userData,[event.target.name]:event.target.value})
+      
+            
  
       }
   return (
@@ -23,7 +49,10 @@ const SimRegister = () => {
                   <label style={{marginLeft:"10%"}}>Email*</label><br/>
                   <input  style={{marginLeft:"10%"}}type='email'required name='email' onChange={handleChange}  value={userData.email}/><br/>
                   <label style={{marginLeft:"10%"}}>Password*</label><br/>
-                  <input style={{marginLeft:"10%"}} type='password'required name='password' onChange={handleChange}  value={userData.password}/><br/>
+                  <input  style={{marginLeft:"10%"}}type='password'required name='password' onChange={handleChange}  value={userData.password}/><br/>
+
+                  <label style={{marginLeft:"10%"}}>Confirm Password *</label><br/>
+                  <input style={{marginLeft:"10%"}} type='password'required name='Confirmpassword' onChange={handleChange}  value={userData.Confirmpassword}/><br/>
                   <input style={{marginLeft:"10%"}} type='submit' value="Register" />
             </fieldset>
       </form>
